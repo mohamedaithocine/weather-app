20bc59ccd65a7ec7a6959c8051844f2128b47788
Mohamed Ait-Hocine <ec22479@qmul.ac.uk>
2024-03-03 15:46:41 +0000

    Implemented the react aspect to it. Barebones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=b167c88bb7301bc04a99e1bd24f7c97c`);
+      setWeatherData(response.data);
+      console.log(response.data); // You can see all the weather data in console log
+    } catch (error) {
+      console.error(error);
+    }
+  }, [city]);
 
-export default App;
+  useEffect(() => {
+  fetchData();
+  }, [fetchData]);
+  const handleInputChange = (e) => {
+  setCity(e.target.value);
+  };
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  fetchData();
+  };
+
+  return (
+    <div>
+    <form onSubmit={handleSubmit}>
+    <input
+    type="text"
+    placeholder="Enter city name"
+    value={city}
+    onChange={handleInputChange}
+    />
+    <button type="submit">Get Weather</button>
+    </form>
+    {weatherData ? (
+    <>
+    <h2>{weatherData.name}</h2>
+    <p>Temperature: {weatherData.main.temp}°C</p>
+    <p>Description: {weatherData.weather[0].description}</p>
+    <p>Feels like : {weatherData.main.feels_like}°C</p>
+    <p>Humidity : {weatherData.main.humidity}%</p>
+    <p>Pressure : {weatherData.main.pressure}</p>
+    <p>Wind Speed : {weatherData.wind.speed}m/s</p>
+    </>
+    ) : (
+    <p>Loading weather data...</p>
+    )}
+    </div>
+    );
+    };
+    export default App;
+
