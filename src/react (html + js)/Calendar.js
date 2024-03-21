4aec76209ab01e2edd44bc71c16f50a5570c1574
Mohamed Ait-Hocine <ec22479@qmul.ac.uk>
2024-03-21 18:57:20 +0000

    Added Ahmed's section

--- src/react (html + js)/Calendar.js
+++ src/react (html + js)/Calendar.js
@@ -1,25 +1,35 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import CalendarItem from "./CalendarItem";
+import CalenderItem from "./CalendarItem";
 
 function Calendar() {
-  const [matches, setMatches] = useState([]);
+  const [matches, setMatches] = useState([]); // state to store matches data
 
+  // Function to fetch weather data for a given location
+  const fetchWeather = async (location) => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=b167c88bb7301bc04a99e1bd24f7c97c`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      return null;
+    }
+  };
+
+  // Function to fetch match data
   const fetchData = async () => {
     const options = {
       method: "GET",
       url: "https://api-football-v1.p.rapidapi.com/v3/fixtures",
       params: {
-        league: "39", // 39 -> English Premier League
-        season: "2023", // Current Season
-        from: "2024-03-20", // Matches for the coming month
+        league: "39",
+        season: "2023",
+        from: "2024-03-20",
         to: "2024-04-01",
         timezone: "Europe/London",
-        status: "NS", // Not Played yet.
+        status: "NS",
       },
       headers: {
-        // Azlan's api key: "1dae4b29e9msh18369a4e952eb73p1f44bfjsnf5cbb3cfb218", exhausted for today
-        // Add your api key below
         "X-RapidAPI-Key": "1dae4b29e9msh18369a4e952eb73p1f44bfjsnf5cbb3cfb218",
         "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
       },
@@ -27,19 +37,28 @@ function Calendar() {
 
     try {
       const response = await axios.request(options);
-      const matchesData = response.data.response.map((match) => ({
-        date: match.fixture.date.split("T")[0],
-        venueId: match.fixture.venue.id,
-        homeTeam: match.teams.home.name,
-        awayTeam: match.teams.away.name,
+      // processing match data and fetch weather for each match
+      const matchesData = await Promise.all(response.data.response.map(async (match) => {
+        const weatherData = await fetchWeather(match.fixture.venue.city);
+        console.log(weatherData)
+        return {
+          date: match.fixture.date.split("T")[0], // extracting date of the match
+          homeTeam: match.teams.home.name, // Home team name
+          homeTeamimg: match.teams.home.logo, // Home team logo
+          awayTeam: match.teams.away.name, // Away team name
+          awayTeamimg: match.teams.away.logo, // Away team logo
+          location: match.fixture.venue.city, // Match location
+          weather: weatherData // Weather data for the match location
+        };
       }));
-      setMatches(matchesData);
+      setMatches(matchesData); // setting matches data in state
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching match data:", error);
     }
   };
+
   useEffect(() => {
-    fetchData();
+    fetchData(); // Fetching data on component mount
   }, []);
 
   return (
@@ -49,14 +68,16 @@ function Calendar() {
       </div>
       <div className="container my-3">
         <h3>Upcoming Matches:</h3>
-        <div className="row justify-content-md-center">
+        <div className="row">
           {matches.slice(0, 9).map((match, index) => (
-            <div className="col md-3 align-self-start" key={index}>
-              <CalendarItem
-                className="h-100 d-inline-block"
+            <div className="col md-4" key={index}>
+              <CalenderItem
                 title={`${match.homeTeam} vs ${match.awayTeam}`}
                 date={match.date}
-                venue={match.venueId}
+                location={match.location}
+                homeTeamimg={match.homeTeamimg}
+                awayTeamimg={match.awayTeamimg}
+                weather={match.weather}
               />
             </div>
           ))}
--- src/react (html + js)/CalendarItem.js
+++ src/react (html + js)/CalendarItem.js
@@ -1,74 +1,29 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import imgNotAvailable from "../images/image_not_available.png";
-function CalendarItem(props) {
-  const [venueData, setVenueData] = useState({
-    name: "",
-    address: "",
-    city: "",
-    country: "",
-    image: "",
-  });
+import React from "react";
+import logo from "../images/premier-league-logo.png";
+import "../css/CalendarItem.css";
 
-  const fetchVenue = async () => {
-    const options = {
-      method: "GET",
-      url: "https://api-football-v1.p.rapidapi.com/v3/venues",
-      params: {
-        id: props.venue,
-      },
-      headers: {
-        // Azlan's api key: "1dae4b29e9msh18369a4e952eb73p1f44bfjsnf5cbb3cfb218", exhausted for today
-        // Add your api key below
-        "X-RapidAPI-Key": "",
-        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-      },
-    };
-    try {
-      const response = await axios.request(options);
-      const data = response.data;
-      if (data && data.response && data.response.length > 0) {
-        const venue = data.response[0];
-        var img = venue.image;
-        if (!img) {
-          img = imgNotAvailable;
-        }
-        const updatedVenueData = {
-          name: venue.name,
-          address: venue.address,
-          city: venue.city,
-          country: venue.country,
-          image: img,
-        };
-        setVenueData(updatedVenueData);
-      } else {
-        console.error("Venue data not found");
-      }
-    } catch (error) {
-      console.error("Error fetching venue data:", error);
-    }
-  };
-  useEffect(() => {
-    fetchVenue();
-    // eslint-disable-next-line
-  }, [props.venueId]);
-
+// CalenderItem component to display match data
+function CalenderItem(props) {
   return (
-    <div className="card my-2" style={{ width: "18rem" }}>
-      <img src={venueData.image} className="card-img-top" alt="" />
-      <div className="card-body">
-        <h5 className="card-title fw-bold">{props.title}</h5>
-        <h5 className="card-title">{props.date}</h5>
-        <p className="card-text">
-          <strong>{venueData.name}</strong>, {venueData.country} <br />
-          {venueData.address}
-        </p>
-        <a href="/#" className="btn btn-primary">
-          See Weather →
-        </a>
+    <>
+      <div className="card my-2" style={{ width: "18rem" , height: "20rem", backgroundColor:"#dafcca"}}>
+        <div className="TeamLogos">
+          <img src={props.homeTeamimg} className="card-img-top" alt="" />
+          <img src={props.awayTeamimg} className="card-img-top" alt="" />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title fw-bold">{props.title}</h5>
+          <h5 className="card-date">{props.date}</h5>
+          <p className="card-text fw-bold location-color">{props.location}</p>
+          <pre>temp: {props.weather.main.temp}{' °C\n'}
+          pressure: {props.weather.main.pressure}{' hPa\n'}
+          humidity: {props.weather.main.humidity}{' %\n'}
+          feels-like: {props.weather.main.temp}{'°C'}
+          </pre>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export default CalendarItem;
+export default CalenderItem;
